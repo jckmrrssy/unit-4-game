44755014496b6a77c7db22537b9b87b13f485bae
Jack Morrissey <j.morrissey.16@gmail.com>
2018-09-24 19:41:48 -0500

    got image selectors working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,27 @@
 // Initializer
+var current = 0;
 $(document).ready(function() {
 // Target number randomizer (initial)
 let targetNumber = Math.floor(Math.random() * (120-19+1)) + 19;
 $('#target').html(targetNumber); 
-// might need to come back to this but we sees 
+// Current Value 
 
 // Array containing four different crystal pictures
-let picOptions = ['assets/images/blue.jpeg','assets/images/green.jpeg','assets/images/orange.jpg','assets/images/purple.jpeg',];
-// Looping through index 0-3 to append crystal pic and assign attributes
+let picOptions = ['assets/images/blue.jpeg','assets/images/green.jpeg', 'assets/images/orange.jpeg', 'assets/images/purple.jpeg',];
+// Looping through index 0-3 to append crystal pic and assign random value 1-12
 for (i=0; i < picOptions.length; i++) {
     var imageCrystal = $('<img>');
-    imageCrystal.addClass(".crystal-image");
+    imageCrystal.addClass("crystal-image");
     imageCrystal.attr("src", picOptions[i]);
     imageCrystal.attr('data-crystalvalue', Math.floor(Math.random()* (12-1+1))+1);
-    $('#empty').append(imageCrystal);
+    $("#notTaken").append(imageCrystal);
 }
+// When you click a crystal, it will add its value to your current score
+$(".crystal-image").on("click", function() {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    current += crystalValue;
+    $("#current").html(current);
+})
+
 });
