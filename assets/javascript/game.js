68bf8a3511d69784f72ae13f187513cf50748b84
Jack Morrissey <j.morrissey.16@gmail.com>
2018-09-24 20:50:17 -0500

    Target, Current, Wins & Losses now working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,15 @@
-// Initializer
-var current = 0;
+
+// Initializer 
 $(document).ready(function() {
+
+var current = 0;  
 // Target number randomizer (initial)
-let targetNumber = Math.floor(Math.random() * (120-19+1)) + 19;
+var targetNumber = Math.floor(Math.random() * (120-19+1)) + 19;
 $('#target').html(targetNumber); 
 // Current Value 
 
 // Array containing four different crystal pictures
-let picOptions = ['assets/images/blue.jpeg','assets/images/green.jpeg', 'assets/images/orange.jpeg', 'assets/images/purple.jpeg',];
+var picOptions = ['assets/images/blue.jpeg','assets/images/green.jpeg', 'assets/images/orange.jpeg', 'assets/images/purple.jpeg',];
 // Looping through index 0-3 to append crystal pic and assign random value 1-12
 for (i=0; i < picOptions.length; i++) {
     var imageCrystal = $('<img>');
@@ -16,12 +18,40 @@ for (i=0; i < picOptions.length; i++) {
     imageCrystal.attr('data-crystalvalue', Math.floor(Math.random()* (12-1+1))+1);
     $("#notTaken").append(imageCrystal);
 }
+
+function reset () {
+    targetNumber = Math.floor(Math.random() * (120-19+1)) + 19;
+    current = 0;
+    
+    $("#current").html(current);
+    $("#target").html(targetNumber);
+}
+// Declaring win/loss variables
+var wins = 0;
+var losses = 0;
+
 // When you click a crystal, it will add its value to your current score
 $(".crystal-image").on("click", function() {
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     current += crystalValue;
     $("#current").html(current);
+    // Win - lose conditions
+    if (current === targetNumber) {
+        wins = wins + 1;
+        $("#wins").text(wins);
+        reset ();
+    }
+    else if (current > targetNumber) {
+        losses = losses + 1;
+        $("#losses").text(losses);
+        reset ();
+    }
+
+
 })
 
+
 });
+
+
